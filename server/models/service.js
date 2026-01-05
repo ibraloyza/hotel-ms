2d4cff2f90f7202f3503060c1b6b5ace84b48402
ibraloyza <ibraahim3523@gmail.com>
2026-01-05 18:07:20 +0300

    added service  model

---
+++ server/models/service.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+const serviceSchema =  new mongoose.Schema({
+    serviceName: {type:String,required: true,unique:true},
+    category: {
+        type:String,required:true, 
+        enum: ['Room','Hall','Office'],
+        required:true
+    },
+    type: {
+        type:String,
+        required: true
+    },
+    price: {
+        type:Number,
+        required: true
+    },
+    isAvailable: {
+        type:Boolean,
+        default: true
+    },
+    description: {
+        type:String,
+        required: true
+    }
+},{timestamps:true})
