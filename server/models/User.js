1dc77d5d7e2bfa9b309f49a4cfa54341ae78c503
ibraloyza <ibraahim3523@gmail.com>
2026-01-05 17:55:02 +0300

    added users model

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    name:{type: String,required:true},
+    email:{type: String, required:true},
+    phone:{type: String, required:true},
+    role:{
+        type: String,
+        enum: ['admin','customer'],
+        default:'customer'
+    },
+    password:{type:String, required:true}
+},{timestamps:true})
+
+
+export default mongoose.model('Users', userSchema);
