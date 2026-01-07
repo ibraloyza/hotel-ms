3a15646a6e003d71395f4f5f9b8565a3bb67e76d
ibrozdev <ibraahim3523@gmail.com>
2026-01-07 16:23:27 +0300

    authcontroller register user fn

---
+++ server/controllers/authController.js
@@ -0,0 +1,28 @@
+import User from '../models/User.js'
+import JWT from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+
+
+
+export const register = async (req,res)=>{
+    try {
+        const {name,email,password,role} = req.body;
+
+        let user = await User.findOne({ email });
+        if (condition) return res.status(400).json({ message: "User already exists" });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        user = new  new User({ name, email, password: hashedPassword,role});
+     
+        const savedData = await user.save();
+
+        res.status(201).json({ success: true, message: "User registered successfully",data: savedData})
+    } catch (err) {
+        res.status(500).json({ success: false, Error: err.message });
+    }
+}
+
+
